5062ae1981e51cd432aaa05f762be081f942cbbe
Azizul Islam <azizultushar98@gmail.com>
2024-09-28 22:47:35 +0600

    reservation CRUD

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,6 +1,11 @@
 "use client";
 
-import { isWithinInterval } from "date-fns";
+import {
+  differenceInDays,
+  isPast,
+  isSameDay,
+  isWithinInterval,
+} from "date-fns";
 import { useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
@@ -18,11 +23,12 @@ function isAlreadyBooked(range, datesArr) {
 
 function DateSelector({ cabin, settings, bookedDates }) {
   const { range, setRange, resetRange } = useReservation();
-  // CHANGE
-  const regularPrice = 23;
-  const discount = 23;
-  const numNights = 23;
-  const cabinPrice = 23;
+
+  const displayRange = isAlreadyBooked(range, bookedDates) ? {} : range;
+
+  const { regularPrice, discount } = cabin;
+  const numNights = differenceInDays(range.to, range.from);
+  const cabinPrice = numNights * (regularPrice - discount);
 
   // SETTINGS
   const { minBookingLength, maxBookingLength } = settings;
@@ -35,12 +41,16 @@ function DateSelector({ cabin, settings, bookedDates }) {
         min={minBookingLength + 1}
         max={maxBookingLength}
         onSelect={setRange}
-        selected={range}
+        selected={displayRange}
         fromMonth={new Date()}
         fromDate={new Date()}
         toYear={new Date().getFullYear() + 5}
         captionLayout="dropdown"
         numberOfMonths={2}
+        disabled={(currDay) =>
+          isPast(currDay) ||
+          bookedDates.some((date) => isSameDay(date, currDay))
+        }
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,90 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import supabase from "./supabase";
+import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
+
+const nationalIdRegex = /^[a-zA-Z0-9]{6,12}$/;
+
+function validateNationalId(nationalId) {
+  return nationalIdRegex.test(nationalId);
+}
+
+export async function updateGuest(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be looged in!!");
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!validateNationalId(nationalID))
+    throw new Error("Invalid National ID, please provide a valid national ID");
+
+  const updateData = { nationalID, nationality, countryFlag };
+
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user.guestId);
+
+  if (error) throw new Error("Guest could not be updated");
+
+  revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(id) {
+  const session = await auth();
+  if (!session) throw new Error("You must be looged in!!");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingsIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingsIds.includes(id))
+    throw new Error("You are not allowed to deleted this bookings");
+
+  const { error } = await supabase.from("bookings").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be deleted");
+  }
+
+  revalidatePath("/account/reservations");
+}
+
+export async function updateBooking(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in!!");
+
+  const bookingId = Number(formData.get("bookingId"));
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingsIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingsIds.includes(bookingId))
+    throw new Error("You are not allowed to edit this bookings");
+
+  const updateData = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+  };
+
+  const { error } = await supabase
+    .from("bookings")
+    .update(updateData)
+    .eq("id", bookingId)
+    .select()
+    .single();
+
+  if (error) {
+    throw new Error("Booking could not be updated");
+  }
+
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  revalidatePath("/account/reservations");
+  redirect("/account/reservations");
+}
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
