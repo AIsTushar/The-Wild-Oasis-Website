acc158983c82e8368878db210aec1cae42c4441f
Azizul Islam <azizultushar98@gmail.com>
2024-09-30 13:48:32 +0600

    Almost completed!!(fixing bugs)

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -6,10 +6,10 @@ import {
   isSameDay,
   isWithinInterval,
 } from "date-fns";
-import { useState } from "react";
+
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
-import { useReservation } from "./reservationContext";
+import { useReservation } from "@/app/_components/ReservationContext";
 
 function isAlreadyBooked(range, datesArr) {
   return (
